f8a5b68ff58908ed4afda54b9e9760539aab483d
mannbajpai <mannbajpai.grs@gmail.com>
2022-12-06 20:09:56 +0530

    index html changes

---
+++ build/precache-manifest.69da63c961d03128bccfe02622dec835.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f509cf9a04e1335e8f654e9cfaa8b577",
+    "url": "/react-keeper-app/index.html"
+  },
+  {
+    "revision": "4db286bc047176e313b0",
+    "url": "/react-keeper-app/static/js/2.c6db2bb7.chunk.js"
+  },
+  {
+    "revision": "97806caf8e4abb468540",
+    "url": "/react-keeper-app/static/js/main.a55c4c2c.chunk.js"
+  },
+  {
+    "revision": "a96c1da8b0dc90bfa89f",
+    "url": "/react-keeper-app/static/js/runtime-main.9f2a6c28.js"
+  }
+]);
